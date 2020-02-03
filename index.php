3e4d6811564de0c31b0a7c0f45b22c91d661ac6b
Phaitoon Srinil <phaitoon@buu.ac.th>
2020-02-03 16:29:29 +0700

    ใส่ ; เข้าไปในบรรทัด 6-7

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ echo "Hello World!<br>";
 echo "Thailand<br>";
 echo "hi<br>";
 echo "Thanathorn Charoenphon<br>";
-echo "Jarupong Taengsri<br>"
-echo "Phaitoon Srinil<br>"
+echo "Jarupong Taengsri<br>";
+echo "Phaitoon Srinil<br>";
 ?>
